8cb310fb4e4c5ca85be0b67791331d63947aa56c
Valzionn <144949469+Valzionn@users.noreply.github.com>
2023-11-29 18:46:19 +0000

    Starting The Project

---
+++ lokaverkefni/lib/lokaverkefni.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+
+class Player {
+  
+}
+
+class Garfield {
+  String? food;
+  String? ingredients;
+  String? place;
+  String? rooms;
+}
+
+List<String> food = [
+  'Hot Dogs',
+  'Pepperoni Pizza',
+  'Hamburger',
+  'Ice Cream',
+  'Meatloaf',
+  'Steak',
+  'Spaghetti',
+  'raisins',
+  'Fruit Cake',
+  'Spinach',
+  'Yoghurt'
+];
+
+// if food 9 to 12 = stomach problems = vet visit
+
+List<String> ingredients = [
+  'Ground Beef',
+  'Onion',
+  'Salt and Pepper',
+  'Pastasauce',
+  'Lasagna Noodles',
+  'Cheese'
+];
+ 
+List<String> place = [
+
+];
+
+
+void main() {
+  print('You\'re the new cat on the block, and you hear through an open window cries for help');
+}
