7bd4e7ae4eaa03366f6254591c8cd71aac6e66c7
Valzionn <144949469+Valzionn@users.noreply.github.com>
2023-12-07 18:36:35 +0000

    k

--- lokaverkefni/lib/lokaverkefni.dart
+++ lokaverkefni/lib/lokaverkefni.dart
@@ -1,7 +1,12 @@
+import 'dart:html';
 import 'dart:io';
+import 'dart:js_interop';
 
-class Player {}
-
+class Player {
+  List<String> imaginaryBackpack = [];
+  int lives = 3;
+}
+/*
 class Garfield {
   String? food;
   String? ingredients;
@@ -11,8 +16,20 @@ class Garfield {
 
   Garfield({this.food, this.ingredients, this.place, this.rooms, this.blurv});
 }
+*/
+class places {
+  List<String> buildings = [
+  'Jons House',
+  'Hot Dog Trailer',
+  'Burger Joint',
+  'Ice Cream Cart'
+  'Vito\'s pizza'
+];
 
-List<String> food = [
+}
+
+class Restaurants {
+  List<String> food = [
   'Hot Dogs',
   'Pepperoni Pizza',
   'Hamburger',
@@ -24,27 +41,35 @@ List<String> food = [
   'Fruit Cake',
   'Spinach',
   'Yoghurt'
-];
+  'Anchovies'
+  ];
 
-// if food 9 to 12 = stomach problems = vet visit
+  // if food 9 to 13 = stomach problems = vet visit
 
-List<String> ingredients = [
+  List<String> ingredients = [
   'Ground Beef',
   'Onion',
   'Salt and Pepper',
   'Pastasauce',
   'Lasagna Noodles',
   'Cheese'
-];
+  ];
+  // get all = win
+}
 
-List<String> place = [
-  'Jons House',
-  'Hot dog stand',
-  'burger joint',
-  ''
-];
 
-void main(_Garfield) {
+void main(/*_Garfield*/) {
+  print('You wake suddenly to a lion\'s roar, no wait it was your stomach rumbling \n ugh, mondays!,(button?) you roll out of bed and walk to the kitchen');
+  print('huh, this is strange, Jon\'s not home, and there\'s nothing to eat!');
+
+  leave House
+  meet Odie
+  meet Nermal
+  search restaurants
+
+
+
+  /*
   print(
       'You\'re the new cat on the block, and you hear through an open window cries for help, \nit\'s old Garfield, "What\'s Wrong?" you ask, He replies "Jon has left me home alone, on a monday! and there\'s nothing to eat!" \n');
   print(
@@ -54,7 +79,7 @@ void main(_Garfield) {
     } else {
       //game ends
     }
-      
+*/      
 switch(searchHotDogStand) {
       case trashCan;
       break;
@@ -66,5 +91,8 @@ switch(searchHotDogStand) {
     break;
     case 
     }
-    hmmm
+  
+  make Lasagna = win
+  eat goodFood = win
+  lives = 0 = loss (vet visit)
 }
